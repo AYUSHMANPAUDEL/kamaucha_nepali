520392066da76dfde9eb81f1b59e7ca112d3f56f
Ayushman Paudel <mrdark2064@gmail.com>
2024-10-16 22:12:35 +0545

    dashboard half completed

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from home.models import Donation , TotalDonation
+from home.models import Donation , TotalDonation , PayoutRequest
 
 # Register your models here.
 admin.site.register(Donation)
 admin.site.register(TotalDonation)
+admin.site.register(PayoutRequest)
 

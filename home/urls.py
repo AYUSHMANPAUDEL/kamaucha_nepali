d29505cf9b04d8db852656c24c2cabf303c3a13d
Ayushman Paudel <mrdark2064@gmail.com>
2024-10-17 20:06:07 +0545

    basic navigation for the dashboard done

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
    path("login/", views.login_page , name="login_page"),
    path("dashboard/",views.dashboard_page,name="dashboard_page"),
    path("logout/",views.logout_page,name="logout_page"),
+   path("dashboard/support/", views.support_page , name="support_page"),
+   path("dashboard/donation_setup_page/", views.donation_page_setup,name="donation_setup_page"),
+   path("dashboard/alert_setup_page/", views.alert_setup_page,name="alert_setup_page"),
+   path("dashboard/payout_request_page/", views.payout_request_page,name="payout_request_page"),
+   path("dashboard/transcations_history_page",views.transcations_history_page,name="transcations_history_page"),
+   path("dashboard/donations_page",views.donations_page,name="donations_page"),
 ]
--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render , HttpResponse , redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate , login , logout
 from django.contrib import messages
-from home.models import TotalDonation , Donation , PayoutRequest
+from home.models import TotalDonation , Donation , PayoutRequest , Support , Annoucement
 # Create your views here.
 def home_page(request):
     return render(request,"home/index.html")
@@ -49,8 +49,10 @@ def dashboard_page(request):
      except TotalDonation.DoesNotExist:
             total_donation = 0
      payout_requests = PayoutRequest.objects.filter(username=request.user).order_by('-appeal_date')[:5]
+     annoucements = Annoucement.objects.filter().order_by('-created_at')[:5]
+     support_tickets = Support.objects.filter(username=request.user).order_by('-created_at')[:3]
      print(total_donation)
-     return render(request,"home/dashboard.html",{"total_donation":total_donation ,"recent_donations": recent_donations,"payout_requests":payout_requests})
+     return render(request,"home/dashboard.html",{"total_donation":total_donation ,"recent_donations": recent_donations,"payout_requests":payout_requests,"support_tickets":support_tickets,"annoucements":annoucements})
     return redirect("/login/")
 
 def logout_page(request):
@@ -58,3 +60,48 @@ def logout_page(request):
       logout(request)
       return redirect("/login/")
    return redirect("/login/")
+
+def support_page(request):
+    if not request.user.is_authenticated:
+        return redirect("/login/")
+    if request.method == "POST":
+        ticket_title = request.POST.get("title")
+        discord_id = request.POST.get("discordid")
+        support_message = request.POST.get("support_message")
+        print(ticket_title, discord_id, support_message)
+        support_db = Support.objects.create(
+            username=request.user,
+            ticket_title=ticket_title,
+            discord_id=discord_id,
+            support_message=support_message # Assuming you have a status field in your model
+        )
+        support_db.save()
+        messages.success(request, "We have received your message! We will reply to you on Discord shortly.")
+        return redirect("support_page")  # Redirect to prevent resubmission
+    else:
+        # Fetch user's previous support tickets
+        support_tickets = Support.objects.filter(username=request.user).order_by('-created_at')
+        return render(request, "home/support.html", {'support_tickets': support_tickets})
+
+def donation_page_setup(request):
+    if request.user.is_authenticated:
+        return render(request,"home/donation_setup_page.html")
+    return redirect("/login/")
+
+def alert_setup_page(request):
+    if request.user.is_authenticated:
+        return render(request,"home/alert_setup.html")
+def payout_request_page(request):
+    if request.user.is_authenticated:
+        return render(request,"home/payout_request.html")
+    return redirect("/login/")
+    
+def transcations_history_page(request):
+    if request.user.is_authenticated:
+        return render(request,"home/transcations_history.html")
+    return redirect("/login/")
+
+def donations_page(request):
+    if request.user.is_authenticated:
+        return render(request,"home/donations.html")
+    return redirect("/login/")
